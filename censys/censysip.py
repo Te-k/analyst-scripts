2b60aae936036c8c2d4d515812bfa85a69090c69
Tek <tek@randhome.io>
2017-01-13 16:29:58 -0500

    Censys IP cli tool

---
+++ censys/censysip.py
@@ -0,0 +1,41 @@
+import censys.ipv4
+import os
+import argparse
+import json
+from censyslib import *
+
+
+def get_ipv4records(censys):
+    series = c.get_series_details("ipv4")
+    return series["tables"]
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Request censys IPv4 database')
+    parser.add_argument('--search', '-s', help='Search term in Censys database')
+    parser.add_argument('--ip', '-i', help='Check info on the given IP')
+    parser.add_argument('--max-results', '-m', default=100, type=int, help='Max number of results')
+    parser.add_argument('--verbose', '-v', action='store_true', help='Verbose mode')
+    args = parser.parse_args()
+
+    key = get_apikey()
+
+    cc = censys.ipv4.CensysIPv4(api_id=key[0], api_secret=key[1])
+
+    if args.search is not None:
+        it = cc.search(args.search)
+        results = []
+        try:
+            for i in range(args.max_results):
+                results.append(it.next())
+        except StopIteration:
+                pass
+
+        # print IP list
+        for ip in results:
+            print(ip['ip'])
+    elif args.ip is not None:
+        ip = cc.view(args.ip)
+        print(json.dumps(ip, sort_keys=True, indent=4, separators=(',', ': ')))
+    else:
+        parser.print_help()
+
