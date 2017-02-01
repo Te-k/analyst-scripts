a54958297a8311f71e1c830e0d4765ca54cc9ba9
Tek <tek@randhome.io>
2017-02-01 15:06:39 -0500

    add misp2sig (only gmails search for now)

---
+++ misp/misp2sig.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+import argparse
+import sys
+import ConfigParser
+from collections import Counter
+from misp import MispServer, MispEvent
+from misplib import parse_config
+
+"""Tool to create signatures from MISP events
+Author : Tek <tek@randhome.io>
+Date : 01/02/2017
+"""
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Command line interface to MISP servers')
+    parser.add_argument('--server', '-s',  help='Server used for the request', required=True)
+    parser.add_argument('--event', '-e',  help='Event infos', type=int, required=True)
+    parser.add_argument('--dst', '-d',  choices=['gmailsearch'], required=True,
+            help='Search for attributes of this type')
+    args = parser.parse_args()
+
+    config = parse_config()
+    if args.server is not None:
+        if args.server.lower() in config.keys():
+            server = MispServer(url=config[args.server.lower()]['url'],
+                    apikey=config[args.server.lower()]['key'],
+                    ssl_chain=False)
+        else:
+            print("Server not found, quitting...")
+            sys.exit(1)
+
+    else:
+        if 'default' not in config.keys():
+            print("No default severs in MISP conf, quitting...")
+            sys.exit(1)
+        else:
+            server = MispServer(url=config['default']['url'],
+                    apikey=config['default']['key'],
+                    ssl_chain=False)
+
+    if args.dst == "gmailsearch":
+        event = server.events.get(args.event)
+        attributes = filter(
+            lambda x:x.type in ['domain', 'email-src', 'email-subject'],
+            event.attributes
+        )
+        print " OR ".join(map(lambda x: '"' + x.value + '"', attributes))
