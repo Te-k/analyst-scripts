cd23439d50a2f03bf0d4da4f6b5d569206d7e57e
Tek <tek@randhome.io>
2017-11-07 12:00:55 -0500

    Improve check_fun_files

--- httptest/check_fun_files.py
+++ httptest/check_fun_files.py
@@ -1,13 +1,17 @@
 #! /usr/bin/env python2
-import urllib2
-import urlparse
+try:
+    import urlparse
+except ImportError:
+    # python 3
+    import urllib.parse as urlparse
 import argparse
+import requests
 import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Check fun files on the web server')
     parser.add_argument('-f', '--file', help='File which contain the list of common files (default is fun_files.txt)', default='fun_files.txt')
-    parser.add_argument('-n', '--no-save', help='Do not save file found', action='store_true')
+    parser.add_argument('-s', '--save', help='Save file found', action='store_true')
     parser.add_argument('-o', '--outdir', help='Output directory (default is files)', default='files')
     parser.add_argument('-v', '--verbose', help='verbose mode', action="count", default=0)
     parser.add_argument('host', metavar='HOST',  help='Host targeted')
@@ -20,49 +24,45 @@ if __name__ == "__main__":
         host = args.host
         hostname = urlparse.urlparse(args.host).netloc
 
+    headers = {
+        "User-Agent": "Baiduspider+(+http://www.baidu.com/search/spider.htm)"
+    }
+
     # Check host validity
-    req = urllib2.Request(host)
     try:
-        r = urllib2.urlopen(req)
-        if r.getcode() != 200:
-            print "Host unavailable!"
-            exit(1)
-    except urllib2.URLError as e:
-        print "Host unavailable!"
+        r = requests.get(host, headers=headers)
+    except requests.ConnectionError:
+        print("Host unavailable!")
+        exit(1)
+    if r.status_code != 200:
+        print("Bad HTTP code when requesting / (%i), quitting" % r.status_code)
         exit(1)
 
-    if not args.no_save:
+    if args.save:
         # Check dirs
         if not os.path.exists(args.outdir):
             os.makedirs(args.outdir)
         if not os.path.exists(args.outdir + "/" + hostname):
             os.makedirs(args.outdir + "/" + hostname)
 
+
     # Loop on the file
     ffile = open(args.file, "r")
     fname = ffile.readline().strip()
-    found = False
     while fname != "":
-        req = urllib2.Request(urlparse.urljoin(host, fname))
         try:
-            resp = urllib2.urlopen(req)
-            if resp.getcode() == 200:
-                found = True
-                print "%s found ! (%i -> saved)" % (fname, resp.getcode())
-                if not args.no_save:
+            r = requests.get(urlparse.urljoin(host, fname), headers=headers)
+            if r.status_code == 200:
+                if args.save:
+                    print("%s found ! (-> saved)" % fname)
                     fout = open(args.outdir + "/" + hostname + "/" + fname, "a+")
-                    fout.write(resp.read())
+                    fout.write(r.text)
                     fout.close()
+                else:
+                    print("%s found !" % fname)
             else:
-                print "%s found ! (%i)" % (fname, resp.getcode())
-        except urllib2.HTTPError as e:
-            if e.code != 404:
-                print "%s found! (%i)" % (fname, e.code)
-            else:
-                if args.verbose > 0:
-                    print "%s not available" % fname
+                print("%s not found ! (%i)" % (fname, r.status_code))
+        except requests.ConnectionError:
+            print("%s not available for %s" % (host, fname))
 
         fname = ffile.readline().strip()
-
-    if not found:
-        print "No file found"
