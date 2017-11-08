b10ac8e626ba705af4a7d8f3421ab6eef8f30ef9
Tek <tek@randhome.io>
2017-11-07 22:37:51 -0500

    add mqtt thing

---
+++ mqtt-get.py
@@ -0,0 +1,23 @@
+import paho.mqtt.client as mqtt
+import argparse
+
+
+def on_connect(client, userdata, flags, rc):
+    print("[+] Connection successful")
+    client.subscribe('#', qos = 1)        # Subscribe to all topics
+    client.subscribe('$SYS/#')            # Broker Status (Mosquitto)
+
+def on_message(client, userdata, msg):
+    print('[+] Topic: %s - Message: %s' % (msg.topic, msg.payload))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Try to subscribe to some MQTT servers')
+    parser.add_argument('--port', '-p', type=int, default=1883, help="port")
+    parser.add_argument('SERVER', help="Server IP address")
+    args = parser.parse_args()
+
+    client = mqtt.Client(client_id = "MqttClient")
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(args.SERVER, args.port, 60)
+    client.loop_forever()
