ebcd6fb0624fbe05f5fe7bdf6ddfa1d7581e3e0a
Tek <tek@randhome.io>
2019-08-15 02:07:08 +0200

    add yaradex

--- android/README.md
+++ android/README.md
@@ -4,3 +4,4 @@ Scripts relying mostly on [androguard](https://github.com/androguard/androguard)
 
 * `get_package_name.py` : extract package name
 * `get_dex.py` : extrac the classes.dex file
+* `yaradex.py` : run a yara rule over the dex classes of an APK file
---
+++ android/yaradex.py
@@ -0,0 +1,33 @@
+import yara
+import os
+import sys
+import argparse
+from androguard.core.bytecodes.apk import APK
+from androguard.core import androconf
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("YARARULE", help="Path of the yara rule file")
+    parser.add_argument("PATH", help="Path of the executable to check")
+    args = parser.parse_args()
+
+
+    if not os.path.isfile(args.PATH):
+        print("Invalid snoopdroid dump path")
+        sys.exit(-1)
+    if not os.path.isfile(args.YARARULE):
+        print("Invalid path for yara rule")
+        sys.exit(-1)
+
+    if androconf.is_android(args.PATH) != "APK":
+        print("This is not an APK file")
+        sys.exit(-1)
+
+    rules = yara.compile(filepath=args.YARARULE)
+
+    apk = APK(args.PATH)
+    dex = apk.get_dex()
+    res = rules.match(data=dex)
+    if len(res) > 0:
+        print("Matches: {}".format(", ".join([r.rule for r in res])))
