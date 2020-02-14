c6ca25845886ec7fdcf884e5e9cbd44c66e821c8
Tek <tek@randhome.io>
2020-02-14 12:06:08 +0100

    improve petimeline

--- pe/petimeline.py
+++ pe/petimeline.py
@@ -1,59 +1,76 @@
 #! /usr/bin/python2
 import argparse
-import pefile
+import lief
 import os
 import datetime
-from struct import unpack
-from binascii import hexlify
-from time import gmtime, strftime
+import magic
+import pefile
+import hashlib
 
-def extract_datetime(fpath):
+
+def extract_datetime(pe):
     """
-    take a file and return its timestamp
-    based on https://gist.github.com/geudrik/03152ba1a148d9475e81
+    Extract PE timestamp using lief
     """
-    try:
-        handle = open(fpath, 'rb')
-        if hexlify(handle.read(2)) != hexlify(u'MZ'):
-            handle.close()
-            return
-    except:
-        return
+    if pe.header.time_date_stamps:
+        return datetime.datetime.fromtimestamp(pe.header.time_date_stamps)
+    else:
+        return None
 
-    try:
-        handle.seek(60, 0)
-        offset = handle.read(4)
-        offset = hexlify(offset[::-1])
 
-        if offset == '':
-            handle.close()
-            return
+def extract_sig_startdate(pe):
+    if pe.has_signature:
+        issuer_serial = ":".join(map(lambda e : "{:02x}".format(e), pe.signature.signer_info.issuer[1]))
+        for c in pe.signature.certificates:
+            serial = ":".join(map(lambda e : "{:02x}".format(e), c.serial_number))
+            if serial == issuer_serial:
+                d = c.valid_from
+                return datetime.datetime(year=d[0], month=d[1], day=d[2], hour=d[3], minute=d[4], second=d[5])
+    else:
+        return None
 
-        offset = int(offset, 16)
-        handle.seek(offset+8, 0)
-        dword = handle.read(4)
-        handle.close()
 
-        t = unpack(">L", dword[::-1])[0]
-    except:
-        return
-    return datetime.datetime.fromtimestamp(t)
+def get_sha256(fpath):
+    with open(fpath,"rb") as f:
+        res = hashlib.sha256(f.read()).hexdigest()
+    return res
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Create a timeline of PE/DLL timestamp')
     parser.add_argument('DIRECTORY',  help='an integer for the accumulator')
+    parser.add_argument('--recursive', '-r', action='store_true', help='an integer for the accumulator')
 
     args = parser.parse_args()
 
     allfiles = {}
+    mime = magic.Magic(mime=True)
 
-    for root, dirs, files in os.walk(args.DIRECTORY):
-        for f in files:
-            if f[-4:].lower() in [".exe", ".dll"]:
-                timestamp = extract_datetime(os.path.join(root, f))
-                if timestamp is not None:
-                    allfiles[timestamp] = os.path.join(root, f)
+    if args.recursive:
+        for root, dirs, files in os.walk(args.DIRECTORY):
+            for f in files:
+                fpath = os.path.join(root, f)
+                if mime.from_file(fpath) == "application/x-dosexec":
+                    pe = lief.parse(fpath)
+                    timestamp = extract_datetime(pe)
+                    if timestamp is not None:
+                        allfiles[timestamp] = ("TIMESTAMP", fpath, get_sha256(fpath))
+                    timestamp = extract_sig_startdate(pe)
+                    if timestamp is not None:
+                        allfiles[timestamp] = ("SIGNATURE", fpath, get_sha256(fpath))
+    else:
+        for f in os.listdir(args.DIRECTORY):
+            fpath = os.path.join(args.DIRECTORY, f)
+            if os.path.isfile(fpath):
+                if mime.from_file(fpath) == "application/x-dosexec":
+                    pe = lief.parse(fpath)
+                    timestamp = extract_datetime(pe)
+                    if timestamp is not None:
+                        allfiles[timestamp] = ("TIMESTAMP", fpath, get_sha256(fpath))
+                    timestamp = extract_sig_startdate(pe)
+                    if timestamp is not None:
+                        allfiles[timestamp] = ("SIGNATURE", fpath, get_sha256(fpath))
+
 
     dates = sorted(allfiles.keys())
     for d in dates:
-        print("%s - %s" % (d.strftime("%Y-%m-%d %H:%M:%S"), allfiles[d]))
+        print("{} - {} - {} - {}".format(d.strftime("%Y-%m-%d %H:%M:%S"), allfiles[d][0], allfiles[d][2], allfiles[d][1]))
